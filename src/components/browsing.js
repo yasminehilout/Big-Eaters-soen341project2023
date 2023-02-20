b9fe2c209c360934612e97c03883255b6149aadc
yasminehilout <yasmine.hilout@gmail.com>
2023-02-20 12:51:47 -0500

    Set up the firebase with the appropriate imports and exports. Wrote the browsing and auth as components to simplify the App.

---
+++ src/components/browsing.js
@@ -0,0 +1,125 @@
+import { db, auth, storage } from "../config/firebase";
+import { useEffect, useState } from 'react';
+import { getDocs, collection, addDoc, deleteDoc, updateDoc, doc } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
+
+export const Browsing = () => {
+
+    const [jobList, setJobList] = useState([]);
+
+    // New Job States
+    const [newJobTitle, setNewJobTitle] = useState("");
+    const [newYearOfStart, setNewYearOfStart] = useState(0);
+    const [needCoop, setNeedCoop] = useState(false);
+
+    // Update Title State
+    const [updatedTitle, setUpdatedTitle] = useState("");
+
+    // File Upload State
+    const [fileUpload, setFileUpload] = useState(null);
+
+    const jobsCollectionRef = collection(db, "jobs");
+
+    const getJobList = async () => {
+        try {
+            const data = await getDocs(jobsCollectionRef);
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            setJobList(filteredData);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        getJobList();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const onSubmitJob = async () => {
+        try {
+            await addDoc(jobsCollectionRef, {
+                title: newJobTitle,
+                yearOfStart: newYearOfStart,
+                needCoop: needCoop,
+                userId: auth?.currentUser?.uid,
+            });
+            getJobList();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const deleteJob = async (id) => {
+        const jobDoc = doc(db, "jobs", id);
+        await deleteDoc(jobDoc);
+        getJobList();
+    };
+
+    const updateJobTitle = async (id) => {
+        const jobDoc = doc(db, "jobs", id);
+        await updateDoc(jobDoc, { title: updatedTitle });
+        getJobList();
+    };
+
+    const uploadFile = async () => {
+        if (!fileUpload) return;
+        const filesFolderRef = ref(storage, 'projectFiles/fileUpload.name');
+        try {
+            await uploadBytes(filesFolderRef, fileUpload);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                <input
+                    placeholder="Job title..."
+                    onChange={(e) => setNewJobTitle(e.target.value)}
+                />
+                <input
+                    placeholder="Year Of Start..."
+                    type="number"
+                    onChange={(e) => setNewYearOfStart(Number(e.target.value))}
+                />
+                <input
+                    type="checkbox"
+                    checked={needCoop}
+                    onChange={(e) => setNeedCoop(e.target.checked)}
+                />
+                <label> Need Coop </label>
+                <button onClick={onSubmitJob}> Submit Job</button>
+            </div>
+
+            <div>
+                {jobList.map((job) => (
+                    <div>
+                        <h1 style={{ color: job.needCoop ? "green" : "red" }}>
+                            {job.title}
+                        </h1>
+                        <p> Year Of Start: {job.yearOfStart} </p>
+
+                        <button onClick={() => deleteJob(job.id)}> Delete This Job</button>
+
+                        <input
+                            placeholder="new title..."
+                            onChange={(e) => setUpdatedTitle(e.target.value)}
+                        />
+                        <button onClick={() => updateJobTitle(job.id)}> Update Title</button>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <input
+                    type="file"
+                    onChange={(e) => setFileUpload(e.target.files[0])} />
+                <button onClick={uploadFile}> Upload File</button>
+            </div>
+        </div>
+    );
+};
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyDzK_rT73XCYPNOw0soKnw5jyEPkCjW2YQ",
   authDomain: "big-eaters.firebaseapp.com",
@@ -16,9 +14,7 @@ const firebaseConfig = {
   measurementId: "G-K9Q6400LJR"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
