48291d974e6b6f32a4d8f5eaea6a9cc7303cec8e
yasminehilout <yasmine.hilout@gmail.com>
2023-02-27 11:16:39 -0500

    Update browsing.js
    Change the for label for an htmlFor label

--- src/components/browsing.js
+++ src/components/browsing.js
@@ -93,7 +93,7 @@ export const Browsing = () => {
                     placeholder="Job title..."
                     onChange={(e) => setNewJobTitle(e.target.value)}
                 />
-                <label for="seasons">Choose a work season:</label>
+                <label htmlFor="seasons">Choose a work season:</label>
                 <select name="seasons" id="seasons" onChange={(e) => setNewSeason(e.target.value)}>
                     <option value="Fall">Fall</option>
                     <option value="Winter">Winter</option>
