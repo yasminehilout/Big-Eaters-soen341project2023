506b934037ec8564650ab0e05a6362ecc733e957
yasminehilout <yasmine.hilout@gmail.com>
2023-03-09 10:41:25 -0500

    Fixed a mistake in uploadFile.js

--- src/components/uploadFile.js
+++ src/components/uploadFile.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { storage } from "../config/firebase";
 import { ref, uploadBytes } from "firebase/storage";
-import { useState } from 'react';
 
 
 export const UploadFile = () => {
