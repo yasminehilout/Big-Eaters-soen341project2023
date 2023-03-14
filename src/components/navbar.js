475e80fe5d631d55476782efdf89a39a640b74d5
Scott McDonald <smcdo0211@gmail.com>
2023-03-14 18:06:05 -0400

    Centred the icons and swapped order

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ export const Navbar = () => {
 
     return (
         <div className="auth-div" align="right">
-            {user ? <><ProfileMenu /><LogoutMenu /></> : <LoginMenu />}
+            {user ? <><LogoutMenu /><ProfileMenu /></> : <LoginMenu />}
         </div>
     )
 }
