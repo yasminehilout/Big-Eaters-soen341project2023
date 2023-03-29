d0a6812e1572c462c08a3d69f02a71f4d63f6f94
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2023-03-29 00:07:10 -0400

    Removed redundant code and whitespaces

--- src/components/employer-profile.js
+++ src/components/employer-profile.js
@@ -1,6 +1,6 @@
 import { updateDoc, doc, } from 'firebase/firestore';
 import { useState } from 'react';
-import { db, storage } from "../config/firebase";
+import { db } from "../config/firebase";
 import { getAuth } from "firebase/auth";
 import React from 'react'
 import Modal from 'react-modal'
@@ -9,10 +9,10 @@ import PersonIcon from "@mui/icons-material/Person";
 
 import "./css/student-profile.css";
 <link rel="stylesheet"
-  href="https://fonts.googleapis.com/css?family=Sora"></link>
+    href="https://fonts.googleapis.com/css?family=Sora"></link>
 
-  export const EmployerProfile = () =>  {
-    
+export const EmployerProfile = () => {
+
     const auth = getAuth();
     //const user = auth.currentUser;
 
@@ -21,8 +21,8 @@ import "./css/student-profile.css";
     const [isOpen, setIsOpen] = useState(false)
 
     const editProfile = async () => {
-        auth.onAuthStateChanged( async (user) => {
-            if(user) {
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
                 console.log("user signed in", user.uid, newCompanyName, newYears)
                 const employerprofileDocRef = doc(db, "employerProfile", user.uid);
                 await updateDoc(employerprofileDocRef, {
@@ -36,50 +36,50 @@ import "./css/student-profile.css";
         })
     };
 
-    return(
-        <div>  
-            <button className="profileBtn" onClick={() => setIsOpen(true)}><PersonIcon style={{fontSize:'small'}}/></button>
-        <Modal className='profile' isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+    return (
+        <div>
+            <button className="profileBtn" onClick={() => setIsOpen(true)}><PersonIcon style={{ fontSize: 'small' }} /></button>
+            <Modal className='profile' isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
 
-        <div className='modalBackground'>
-            <div className='modalContainer'>
-            <div className='titleCloseBtn'>
-                <button className='xBtn' onClick={() =>  setIsOpen(false)} > X </button> 
-         </div>
-                <div className='title'>
-                    <h1>Edit Posting</h1>
+                <div className='modalBackground'>
+                    <div className='modalContainer'>
+                        <div className='titleCloseBtn'>
+                            <button className='xBtn' onClick={() => setIsOpen(false)} > X </button>
+                        </div>
+                        <div className='title'>
+                            <h1>Edit Posting</h1>
+                        </div>
+                        <div className='body'>
+                            <form>
+                                <input
+                                    className='textBox'
+                                    type="text"
+                                    maxLength="20"
+                                    placeholder="Company Name"
+                                    required
+                                    value={newCompanyName}
+                                    onChange={(e) => setCompanyName(e.target.value)}
+                                />
+                                <div class='underline'></div>
+
+                                <input
+                                    className='textBox'
+                                    type="checkbox"
+                                    placeholder="Company Age"
+                                    required
+                                    value={newYears}
+                                    onChange={(e) => setYears(e.target.value)}
+                                />
+
+                                <div className='footer'>
+                                    <button className="endBtn" onClick={() => editProfile()}>Save</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </div>
-    <div className='body'>
-        <form>
-            <input 
-                className='textBox'
-                type="text"
-                maxLength="20"
-                placeholder = "Company Name"
-                required
-                value={newCompanyName}
-                onChange={(e) => setCompanyName(e.target.value)}
-            />
-            <div class='underline'></div>
-            
-            <input
-            className='textBox'
-                type="checkbox"
-                placeholder = "Company Age"
-                required
-                value={newYears}
-                onChange={(e) => setYears(e.target.value)}
-            />
 
-            <div className='footer'>
-            <button className="endBtn" onClick={() => editProfile()}>Save</button>  
-            </div>
-            </form>
-        </div> 
-    </div>
-</div>
-</Modal>
-</div>   
-
-);
-  }
+    );
+}
