684c285e4d8115597fcdc42b6ef522d0c647ddf0
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2023-04-09 19:52:27 -0400

    Added act() method to fix rendering

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,14 +3,17 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import App from '../App';
+import { act } from 'react-dom/test-utils';
 
 describe('Testing the main menu App component', () => {
   test('renders Navbar and Browsing components', () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
+    act(() => {
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+    });
     expect(screen.getByRole('navigation')).toBeInTheDocument(); // checks if Navbar component is rendered
     expect(screen.getByRole('main')).toBeInTheDocument(); // checks if Browsing component is rendered
   });
