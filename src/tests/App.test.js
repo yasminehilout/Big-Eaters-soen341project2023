9d0812bd858b47d52338d020dc601d4eb2ed953d
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2023-04-09 19:41:40 -0400

    Fixed react-redux element missing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
 import App from '../App';
 
 describe('Testing the main menu App component', () => {
   test('renders Navbar and Browsing components', () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(screen.getByRole('navigation')).toBeInTheDocument(); // checks if Navbar component is rendered
     expect(screen.getByRole('main')).toBeInTheDocument(); // checks if Browsing component is rendered
   });
