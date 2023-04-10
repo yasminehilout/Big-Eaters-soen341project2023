99cbd9c5fcbb0a31a79028685cf43e4c610c71f4
yasminehilout <yasmine.hilout@gmail.com>
2023-04-10 18:20:58 -0400

    Surrounded the rendering of browsing component with act()

--- src/tests/Browsing.test.js
+++ src/tests/Browsing.test.js
@@ -3,15 +3,18 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import { Browsing } from '../components/browsing';
+import { act } from 'react-dom/test-utils';
 
 describe('Browsing component', () => {
     test('renders browsing div', () => {
         // useAuthState.mockReturnValueOnce([null]);
+        act(() => {
         render(
             <Provider store={store}>
                 <Browsing />
             </Provider>
         );
+        });
         const element = document.querySelector('.browsing-div');
 
         // assert that the element exists and has the expected content
