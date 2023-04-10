da80722a861381769fe0ddef6efd6902622ccb01
yasminehilout <yasmine.hilout@gmail.com>
2023-04-10 18:15:07 -0400

    Adding browsing react unit test

---
+++ src/tests/Browsing.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+import { Browsing } from '../components/browsing';
+
+describe('Browsing component', () => {
+    test('renders browsing div', () => {
+        // useAuthState.mockReturnValueOnce([null]);
+        render(
+            <Provider store={store}>
+                <Browsing />
+            </Provider>
+        );
+        const element = document.querySelector('.browsing-div');
+
+        // assert that the element exists and has the expected content
+        expect(element).toBeDefined();
+    });
+});
