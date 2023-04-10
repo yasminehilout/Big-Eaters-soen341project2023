a93998fdabe6cc7f29decc660772bda54efe2c03
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2023-04-09 20:59:37 -0400

    New variant of Navbar test by mockReturnValueOnce

--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -3,54 +3,53 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import { Navbar } from '../components/navbar';
+import { auth } from '../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getRole } from '../features/counter/profileSlice';
+import { useSelector } from 'react-redux';
 
-jest.mock('../config/firebase', () => ({
-  auth: {
-    onAuthStateChanged: jest.fn(),
-    currentUser: null
-  }
-}));
+jest.mock('../config/firebase');
+jest.mock('react-firebase-hooks/auth');
+jest.mock('../features/counter/profileSlice');
+jest.mock('react-redux');
 
 describe('Navbar component', () => {
-  test('renders LoginMenu when user is not authenticated', () => {
-    render(
-      <Provider store={store}>
-        <Navbar />
-      </Provider>
-    );
-    expect(screen.getByText('Sign in as a Student')).toBeInTheDocument();
-    expect(screen.queryByText('Logout')).not.toBeInTheDocument();
-  });
+    test('renders LoginMenu when user is not authenticated', () => {
+        useAuthState.mockReturnValueOnce([null]);
+        render(
+            <Provider store={store}>
+                <Navbar />
+            </Provider>
+        );
+        expect(screen.getByText('Sign in as a Student')).toBeInTheDocument();
+        expect(screen.queryByText('Logout')).not.toBeInTheDocument();
+    });
 
-  test('renders LogoutMenu and StudentProfile when user is authenticated and role is student', () => {
-    jest.mock('../features/counter/profileSlice', () => ({
-      getRole: jest.fn(() => 'student')
-    }));
+    test('renders LogoutMenu and StudentProfile when user is authenticated and role is student', () => {
+        const role = 'student';
+        useSelector.mockReturnValueOnce(role);
+        useAuthState.mockReturnValueOnce([{ uid: '123', email: 'test@test.com' }]);
+        const { container } = render(
+            <Provider store={store}>
+                <Navbar />
+            </Provider>
+        );
+        expect(screen.getByText('Logout')).toBeInTheDocument();
+        expect(screen.queryByText('Sign in as a Student')).not.toBeInTheDocument();
+        expect(container.querySelector('.student-profile')).toBeInTheDocument();
+    });
 
-    const { container } = render(
-      <Provider store={store}>
-        <Navbar />
-      </Provider>
-    );
-
-    expect(screen.getByText('Logout')).toBeInTheDocument();
-    expect(screen.queryByText('Sign in as a Student')).not.toBeInTheDocument();
-    expect(container.querySelector('.student-profile')).toBeInTheDocument();
-  });
-
-  test('renders LogoutMenu and EmployerProfile when user is authenticated and role is employer', () => {
-    jest.mock('../features/counter/profileSlice', () => ({
-      getRole: jest.fn(() => 'employer')
-    }));
-
-    const { container } = render(
-      <Provider store={store}>
-        <Navbar />
-      </Provider>
-    );
-
-    expect(screen.getByText('Logout')).toBeInTheDocument();
-    expect(screen.queryByText('Sign in as an Employer')).not.toBeInTheDocument();
-    expect(container.querySelector('.employer-profile')).toBeInTheDocument();
-  });
+    test('renders LogoutMenu and EmployerProfile when user is authenticated and role is employer', () => {
+        const role = 'employer';
+        useSelector.mockReturnValueOnce(role);
+        useAuthState.mockReturnValueOnce([{ uid: '123', email: 'test@test.com' }]);
+        const { container } = render(
+            <Provider store={store}>
+                <Navbar />
+            </Provider>
+        );
+        expect(screen.getByText('Logout')).toBeInTheDocument();
+        expect(screen.queryByText('Sign in as an Employer')).not.toBeInTheDocument();
+        expect(container.querySelector('.employer-profile')).toBeInTheDocument();
+    });
 });
