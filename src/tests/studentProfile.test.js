e0c12578c8d67b043549b3ce4c62d1d209c288f4
yasminehilout <yasmine.hilout@gmail.com>
2023-04-10 20:49:30 -0400

    Added the student profile button test

---
+++ src/tests/studentProfile.test.js
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import { StudentProfile } from '../components/student-profile';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+
+describe('StudentProfile', () => {
+    test('should render student profile button', async () => {
+        act(() => {
+            render(
+                <Provider store={store}>
+                    <StudentProfile />
+                </Provider>
+            );
+        });
+        const element = document.querySelector('.profileBtn student-profile');
+        
+        expect(element).toBeDefined();
+    });
+});
